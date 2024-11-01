c7621f9ceeef8879456d34df1216c0cba4f05d80
Kamil Śliwak <kamil.sliwak@codepoets.it>
2024-11-01 01:19:18 +0100

    Refactor external benchmark downloader to reduce duplication

--- test/benchmarks/external-setup.sh
+++ test/benchmarks/external-setup.sh
@@ -40,56 +40,41 @@ function neutralize_via_ir {
     sed -i '/^via_ir\s*=.*$/d' foundry.toml
 }
 
-mkdir -p "$BENCHMARK_DIR"
-cd "$BENCHMARK_DIR"
+function setup_foundry_project {
+    local subdir="$1"
+    local ref_type="$2"
+    local ref="$3"
+    local repo_url="$4"
+    local install_function="${5:-}"
 
-if [[ ! -e openzeppelin/ ]]; then
-    git clone --depth=1 https://github.com/OpenZeppelin/openzeppelin-contracts openzeppelin/ --branch v5.0.2
-    pushd openzeppelin/
-    forge install
-    neutralize_via_ir
-    popd
-else
-    echo "Skipped openzeppelin/. Already exists."
-fi
+    printf ">>> %-22s | " "$subdir"
 
-if [[ ! -e uniswap-v4/ ]]; then
-    git clone --single-branch https://github.com/Uniswap/v4-core uniswap-v4/
-    pushd uniswap-v4/
-    git checkout ae86975b058d386c9be24e8994236f662affacdb # branch main as of 2024-06-06
-    forge install
-    neutralize_via_ir
-    popd
-else
-    echo "Skipped uniswap-v4/. Already exists."
-fi
+    [[ $ref_type == commit || $ref_type == tag ]] || assertFail
 
-if [[ ! -e seaport/ ]]; then
-    git clone --single-branch https://github.com/ProjectOpenSea/seaport
-    pushd seaport/
-    # NOTE: Can't select the tag with `git clone` because a branch of the same name exists.
-    git checkout tags/1.6
-    forge install
+    [[ ! -e "$subdir" ]] || { printf "already exists\n"; return; }
+    printf "downloading...\n\n"
+
+    if [[ $ref_type == tag ]]; then
+        git clone --depth=1 "$repo_url" "$subdir" --branch "$ref"
+        pushd "$subdir"
+    else
+        git clone "$repo_url" "$subdir"
+        pushd "$subdir"
+        git checkout "$ref"
+    fi
+    if [[ -z $install_function ]]; then
+        forge install
+    else
+        "$install_function"
+    fi
+
     neutralize_via_ir
     neutralize_version_pragmas
     popd
-else
-    echo "Skipped seaport/. Already exists."
-fi
+    echo
+}
 
-if [[ ! -e eigenlayer/ ]]; then
-    git clone --depth=1 https://github.com/Layr-Labs/eigenlayer-contracts eigenlayer/ --branch v0.3.0-holesky-rewards
-    pushd eigenlayer/
-    neutralize_via_ir
-    forge install
-    popd
-else
-    echo "Skipped eigenlayer/. Already exists."
-fi
-
-if [[ ! -e sablier-v2/ ]]; then
-    git clone --depth=1 https://github.com/sablier-labs/v2-core sablier-v2/ --branch v1.1.2
-    pushd sablier-v2/
+function install_sablier {
     # NOTE: To avoid hard-coding dependency versions here we'd have to install them from npm
     forge install --no-commit \
         foundry-rs/forge-std@v1.5.6 \
@@ -106,8 +91,14 @@ forge-std/=lib/forge-std/
 solarray/=lib/solarray/
 solady/=lib/solady/
 EOF
-    neutralize_via_ir
-    popd
-else
-    echo "Skipped sablier-v2/. Already exists."
-fi
+}
+
+mkdir -p "$BENCHMARK_DIR"
+cd "$BENCHMARK_DIR"
+
+setup_foundry_project openzeppelin/ tag v5.0.2 https://github.com/OpenZeppelin/openzeppelin-contracts
+setup_foundry_project uniswap-v4/ commit ae86975b058d386c9be24e8994236f662affacdb https://github.com/Uniswap/v4-core
+# NOTE: Can't select the tag with `git clone` because a branch of the same name exists.
+setup_foundry_project seaport/ commit tags/1.6 https://github.com/ProjectOpenSea/seaport
+setup_foundry_project eigenlayer/ tag v0.3.0-holesky-rewards https://github.com/Layr-Labs/eigenlayer-contracts
+setup_foundry_project sablier-v2/ tag v1.1.2 https://github.com/sablier-labs/v2-core install_sablier
