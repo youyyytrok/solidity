2ae6807f49d180d2493637d59ca8e723eca2b3d8
rodiazet <radek.zagorowicz@gmail.com>
2024-12-09 11:23:43 +0100

    eof: Introduce new instruction set for `ext*call`

--- libevmasm/SemanticInformation.cpp
+++ libevmasm/SemanticInformation.cpp
@@ -169,6 +169,26 @@ std::vector<SemanticInformation::Operation> SemanticInformation::readWriteOperat
 		});
 		return operations;
 	}
+	case Instruction::EXTCALL:
+	case Instruction::EXTSTATICCALL:
+	case Instruction::EXTDELEGATECALL:
+	{
+		size_t paramCount = static_cast<size_t>(instructionInfo(_instruction, langutil::EVMVersion()).args);
+		size_t const memoryStartParam = _instruction == Instruction::EXTCALL ? paramCount - 3 : paramCount - 2;
+		size_t const memoryLengthParam = _instruction == Instruction::EXTCALL ? paramCount - 2 : paramCount - 1;
+		std::vector<Operation> operations{
+			Operation{Location::Memory, Effect::Read, memoryStartParam, memoryLengthParam, {}},
+			Operation{Location::Storage, Effect::Read, {}, {}, {}},
+			Operation{Location::TransientStorage, Effect::Read, {}, {}, {}}
+		};
+		if (_instruction == Instruction::EXTDELEGATECALL)
+		{
+			operations.emplace_back(Operation{Location::Storage, Effect::Write, {}, {}, {}});
+			operations.emplace_back(Operation{Location::TransientStorage, Effect::Write, {}, {}, {}});
+		}
+
+		return operations;
+	}
 	case Instruction::CREATE:
 	case Instruction::CREATE2:
 		return std::vector<Operation>{
@@ -380,6 +400,9 @@ bool SemanticInformation::isDeterministic(AssemblyItem const& _item)
 	case Instruction::CALLCODE:
 	case Instruction::DELEGATECALL:
 	case Instruction::STATICCALL:
+	case Instruction::EXTCALL:
+	case Instruction::EXTDELEGATECALL:
+	case Instruction::EXTSTATICCALL:
 	case Instruction::CREATE:
 	case Instruction::CREATE2:
 	case Instruction::GAS:
@@ -475,6 +498,9 @@ SemanticInformation::Effect SemanticInformation::memory(Instruction _instruction
 	case Instruction::LOG4:
 	case Instruction::EOFCREATE:
 	case Instruction::RETURNCONTRACT:
+	case Instruction::EXTCALL:
+	case Instruction::EXTDELEGATECALL:
+	case Instruction::EXTSTATICCALL:
 		return SemanticInformation::Read;
 
 	default:
@@ -514,10 +540,13 @@ SemanticInformation::Effect SemanticInformation::storage(Instruction _instructio
 	case Instruction::SSTORE:
 	case Instruction::EOFCREATE:
 	case Instruction::RETURNCONTRACT:
+	case Instruction::EXTCALL:
+	case Instruction::EXTDELEGATECALL:
 		return SemanticInformation::Write;
 
 	case Instruction::SLOAD:
 	case Instruction::STATICCALL:
+	case Instruction::EXTSTATICCALL:
 		return SemanticInformation::Read;
 
 	default:
@@ -537,10 +566,13 @@ SemanticInformation::Effect SemanticInformation::transientStorage(Instruction _i
 	case Instruction::TSTORE:
 	case Instruction::EOFCREATE:
 	case Instruction::RETURNCONTRACT:
+	case Instruction::EXTCALL:
+	case Instruction::EXTDELEGATECALL:
 		return SemanticInformation::Write;
 
 	case Instruction::TLOAD:
 	case Instruction::STATICCALL:
+	case Instruction::EXTSTATICCALL:
 		return SemanticInformation::Read;
 
 	default:
@@ -559,7 +591,10 @@ SemanticInformation::Effect SemanticInformation::otherState(Instruction _instruc
 	case Instruction::CREATE2:
 	case Instruction::EOFCREATE:
 	case Instruction::RETURNCONTRACT:
+	case Instruction::EXTCALL:
+	case Instruction::EXTDELEGATECALL:
 	case Instruction::SELFDESTRUCT:
+	case Instruction::EXTSTATICCALL:
 	case Instruction::STATICCALL: // because it can affect returndatasize
 		// Strictly speaking, log0, .., log4 writes to the state, but the EVM cannot read it, so they
 		// are just marked as having 'other side effects.'
@@ -606,6 +641,7 @@ bool SemanticInformation::invalidInPureFunctions(Instruction _instruction)
 	case Instruction::NUMBER:
 	case Instruction::PREVRANDAO:
 	case Instruction::GASLIMIT:
+	case Instruction::EXTSTATICCALL:
 	case Instruction::STATICCALL:
 	case Instruction::SLOAD:
 	case Instruction::TLOAD:
@@ -635,6 +671,8 @@ bool SemanticInformation::invalidInViewFunctions(Instruction _instruction)
 	case Instruction::CALL:
 	case Instruction::CALLCODE:
 	case Instruction::DELEGATECALL:
+	case Instruction::EXTCALL:
+	case Instruction::EXTDELEGATECALL:
 	// According to EOF spec https://eips.ethereum.org/EIPS/eip-7620#eofcreate
 	case Instruction::EOFCREATE:
 	// According to EOF spec https://eips.ethereum.org/EIPS/eip-7620#returncontract
